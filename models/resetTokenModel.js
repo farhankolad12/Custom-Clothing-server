080104a2fbfddc4440d5d2dd1775ea8092b9e8cc
Farhan Kolad <98447917+farhankolad12@users.noreply.github.com>
2024-06-18 18:31:02 +0530

    Update resetTokenModel.js

--- models/resetTokenModel.js
+++ models/resetTokenModel.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
+
+function dateNow() {
+  return Date.now() + 86400
+}
+
 const ResetTokensSchema = new mongoose.Schema({
   userId: { type: String },
   token: { type: String },
-  expiredAt: { type: Number, default: Date.now + 86400 },
+  expiredAt: { type: Number, default: dateNow },
 });
 
 module.exports = mongoose.model("ResetTokens", ResetTokensSchema);
